dbcb5063a82e7a288e7fa7ce9ee5ae050bda6ece
Ahinsree B <ahinsree2000@gmail.com>
2025-09-27 13:26:29 +0530

    Add example demonstrating variable types and dynamic typing in Python

---
+++ Python_basics/Weeak_1/variables/4_variables.py
@@ -0,0 +1,12 @@
+#Variable Types and Dynamic Typing
+#Python is dynamically typed - you don't need to declare variable types.
+
+# Variable can change type
+x = 10          # x is integer
+print(type(x))  # <class 'int'>
+
+x = "Hello"     # x is now string
+print(type(x))  # <class 'str'>
+
+x = 3.14        # x is now float
+print(type(x))  # <class 'float'>
