73435790466c122e8074a640050acb5e1181a83c
Ahinsree B <ahinsree2000@gmail.com>
2025-09-27 13:26:07 +0530

    Add example for multiple variable assignment and swapping in Python

---
+++ Python_basics/Weeak_1/variables/3_variables.py
@@ -0,0 +1,16 @@
+#Multiple Variable Assignment
+# Single value to multiple variables
+x = y = z = 0
+
+# Multiple values to multiple variables
+name, age, city = "Bob", 25, "New York"
+
+# Swapping variables
+a = 5
+b = 10
+a, b = b, a  # Now a=10, b=5
+print("Name:", name)
+print("Age:", age)
+print("City:", city)
+print("a:", a)
+print("b:", b)
