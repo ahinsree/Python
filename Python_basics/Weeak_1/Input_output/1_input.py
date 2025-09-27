edacae8cd12ae45b22d6d451bfccedb52dcb887b
Ahinsree B <ahinsree2000@gmail.com>
2025-09-27 13:22:15 +0530

    Add comprehensive variable handling and output examples
    - Introduced various examples demonstrating the use of underscores for large numbers, f-strings with custom objects, and advanced formatting techniques.
    - Implemented ANSI color codes for colored output in terminal applications.
    - Enhanced understanding of memory allocation processes, including object creation, reference assignment, and integer interning.
    - Explained mutable vs immutable objects with practical examples.
    - Provided insights into variable scope, best practices for naming, and dynamic typing in Python.
    - Included user information and shopping cart examples to illustrate practical variable usage.
    - Added detailed comments and diagrams to clarify memory management concepts, garbage collection, and function call memory models.

---
+++ Python_basics/Weeak_1/Input_output/1_input.py
@@ -0,0 +1,6 @@
+#Python input() Function
+#The input() function is a built-in Python function that allows you to get user input from the keyboard. Here's a comprehensive overview:
+#Basic Syntax
+#variable = input(prompt_message)
+name = input("Enter your name: ").center(100,'*').upper()
+print(f"Hello,{name}!")
